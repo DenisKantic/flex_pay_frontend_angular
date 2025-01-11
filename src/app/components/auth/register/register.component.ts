393fa6249f51ac8b4c5f4ccd55637f2acb0c80dc
DenisKantic <denis.kantic18@gmail.com>
2025-01-11 10:05:18 +0100

    added functionality logic, tested and it's working. Configured route to login after success

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -40,6 +40,8 @@ export class RegisterComponent {
   passwordControl = new FormControl('', Validators.required) 
   usernameControl = new FormControl('', Validators.required) 
   cardControl = new FormControl('', Validators.required) 
+  dateControl = new FormControl('', Validators.required) 
+
 
   matcher = new MyErrorStateMatcher();
   hide = signal(true);
@@ -51,24 +53,51 @@ export class RegisterComponent {
     event.stopPropagation();
   }
 
-  login(){
+  formatDate(rawDate: string): string {
+    const date = new Date(rawDate); // Convert the string to a Date object
+    const day = String(date.getDate()).padStart(2, '0'); // Extract day with leading zero
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Extract month (0-based index)
+    const year = date.getFullYear(); // Extract year
+    return `${day}-${month}-${year}`; // Format as dd-mm-yyyy
+  }
+  
+
+  register(){
+
+    let new_date;
+  
+    const rawDate = this.dateControl.value?.toString(); // Get the date as a string
+    if (rawDate) {
+      const formattedDate = this.formatDate(rawDate);
+      new_date = this.formatDate(rawDate)
+      console.log('Formatted Date:', formattedDate);
+    } else {
+      console.log('No date selected');
+    }
+  
     console.log('Login button clicked');
   console.log('Email:', this.EmailControl.value);
   console.log('Password:', this.passwordControl.value);
-    if (this.EmailControl.valid && this.passwordControl.valid){
-      const login_data = {
+  console.log("date", this.dateControl.value)
+    if (this.EmailControl.valid && this.passwordControl.valid
+      && this.usernameControl && this.cardControl && this.dateControl
+    ){
+      const register_data = {
+        name: this.usernameControl.value,
         email: this.EmailControl.value,
-        password: this.passwordControl.value
+        password: this.passwordControl.value,
+        card_num: this.cardControl.value,
+        valid_to: new_date
       }
 
-      axios.post('http://localhost:8080/login', login_data, {withCredentials: true})
+      axios.post('http://localhost:8080/register', register_data, {withCredentials: true})
       .then(response=>{
-        this._snackBar.open('Login successful!', 
+        this._snackBar.open('Register successful!', 
           'Close', 
           { duration: 3000 ,
           panelClass: ['success-snackbar']
        }); 
-       this.router.navigate(['/dashboard'])
+       this.router.navigate(['/login'])
         console.log(response.data)
       })
       .catch(error =>{
